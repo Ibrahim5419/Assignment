b1d2835a40fb151860a2b68a6d196090df47d945
ibrahim5419 <phatanibrahim71865@gmail.com>
2024-08-28 21:16:38 +0530

    Your descriptive commit message

--- config/typeorm.config.js
+++ config/typeorm.config.js
@@ -1,15 +1,16 @@
-// typeorm.config.js
+const path = require("path");
+
 module.exports = {
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "Ibrahim786@",
-  database: "airport_db",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [
-    require("../entities/airport"), // adjust based on the actual file location
+    require("../entities/airport"),
     require("../entities/city"),
     require("../entities/country"),
   ],
